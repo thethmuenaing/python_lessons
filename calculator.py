3a848170dc04e4bbcd17d36e2627e9e05f3a9982
thet hmue naing <thethmuenaing@thets-MacBook-Air.local>
2026-02-13 22:47:36 +0700

    calculator program

---
+++ calculator.py
@@ -0,0 +1,18 @@
+operator = input("Enter an Operator ( + - * /): ")
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+if operator == "+":
+    result = num1 + num2
+    print(round(result))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result))
+else:
+    print(f"{operator} is not a valid operator")
